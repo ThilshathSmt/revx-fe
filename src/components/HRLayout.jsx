aa1d33e6902585ccbc398f0c906d507f82c50647
Faskath513 <faskath9054@gmail.com>
2024-12-21 15:32:43 +0530

    Rename HRlayout

--- src/components/HRLayout.jsx
+++ src/components/HRLayout.jsx
@@ -1,38 +1,36 @@
-// src/components/Layout.jsx
-
 import React from 'react';
 import HRSidebar from './HRSidebar'; // Import HRSidebar
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Box } from '@mui/material';
 
-const Layout = ({ children }) => {
+const HRLayout = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-      {/* Sidebar */}
-      <HRSidebar />
+  
 
       <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         {/* Navbar */}
-        {/* <Navbar /> */}
-
+        <Navbar />
+        <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+      {/* Sidebar */}
+      <HRSidebar />
         <Box
           component="main"
           sx={{
             flex: 1,
             padding: 3,
             backgroundColor: 'background.default',
-            marginLeft: '260px', // Account for the width of the sidebar
+            marginLeft: '260px', // Adjust the margin to match the width of the sidebar
+            overflow: 'auto', // Ensure content is scrollable if it overflows
           }}
         >
           {children}
         </Box>
-
-        {/* Footer */}
-        <Footer />
       </Box>
+      {/* Footer */}
+      <Footer />
     </Box>
   );
 };
 
-export default Layout;
+export default HRLayout;
